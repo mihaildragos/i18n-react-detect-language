5eed7509ea29fc01b60dc4dfac4804062947f0b9
mihaildragos <48980910+mihaildragos@users.noreply.github.com>
2022-10-20 13:51:54 +0100

    initial comit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
+import { useTranslation } from 'react-i18next';
+
 import logo from './logo.svg';
 import './App.css';
 
+const languages = {
+	en: { nativeName: 'English' },
+	de: { nativeName: 'Deutsch' }
+};
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const { t, i18n } = useTranslation();
+
+	document.documentElement.dir = i18n.dir();
+
+
+	return (
+		<div className="App">
+			<div>
+				{Object.keys(languages).map((lng) => (
+					<button
+						key={lng}
+						style={{ fontWeight: i18n.resolvedLanguage === lng ? 'bold' : 'normal' }}
+						type="submit"
+						onClick={() => {
+							i18n.changeLanguage(lng);
+							document.documentElement.lang = i18n.language;
+						}
+					}>
+						{languages[lng].nativeName}
+					</button>
+				))}
+			</div>
+			<h1>{t('description.text1')}</h1>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/i18n.js
@@ -0,0 +1,62 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+const i18nTranslations = {
+    en: {
+        translation: {
+            description: {
+                text1: 'ENGLISH content',
+            }
+        }
+    },
+    de: {
+        translation: {
+            description: {
+                text1: 'GERMAN content'
+            }
+        }
+    }
+}
+
+const detectionOptions = {
+    // order and from where user language should be detected
+    order: [
+        "path",
+        "localStorage",
+        "cookie",
+        "sessionStorage",
+        "navigator",
+        "querystring",
+    ],
+
+    // keys or params to lookup language from
+    lookupQuerystring: "lng",
+    lookupCookie: "selected_language",
+    lookupLocalStorage: "selected_language",
+    lookupSessionStorage: "selected_language",
+    lookupFromPathIndex: 0,
+    lookupFromSubdomainIndex: 0,
+
+    // cache user language on
+    caches: ["localStorage", "cookie"],
+    excludeCacheFor: ["cimode"], // languages to not persist (cookie, localStorage)
+
+    // optional htmlTag with lang attribute, the default is:
+    htmlTag: document.documentElement,
+}
+
+i18n
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+        debug: true,
+        fallbackLng: 'en',
+        interpolation: {
+            escapeValue: false, // not needed for react as it escapes by default
+        },
+        resources: i18nTranslations,
+        detection: detectionOptions,
+    });
+
+export default i18n;
